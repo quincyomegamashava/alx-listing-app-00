bcfeb3c973e58d0480b8f84af00839dc0036fff9
QuincyMashava <mashavaquincy@gmail.com>
2025-07-04 11:13:52 +0200

    new changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    allowedDevOrigins: ['http://192.168.70.80:3000'], // add your IP here
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
-
-export default nextConfig;
+export default nextConfig
